972fa9596ab96a7c12236077c6b9e2fbcfc9760d
Jens-Christian Fischer <jens-christian.fischer@switch.ch>
2018-06-13 20:37:40 +0200

    Fix a number of problems with the Rect functions
    - several cases of mixed x/y, w/h
    - wrong calculation of origin when splitting rectangles
    - More examples

--- src/platform/cocoa/rect.rs
+++ src/platform/cocoa/rect.rs
@@ -24,32 +24,45 @@ impl Rect {
     }
 
     pub fn from_nsrect(rect: NSRect) -> Self {
-        Rect{ origin: Point{ x: rect.origin.y, y: rect.origin.x }, size: Size{ width: rect.size.height, height: rect.size.width }}
+        Rect{ origin: Point{ x: rect.origin.x, y: rect.origin.y }, size: Size{ width: rect.size.width, height: rect.size.height }}
     }
 
     pub fn to_nsrect(&self) -> NSRect {
-        NSRect::new(NSPoint::new(self.origin.y, self.origin.x), NSSize::new(self.size.height, self.size.width))
+        NSRect::new(NSPoint::new(self.origin.x, self.origin.y), NSSize::new(self.size.width, self.size.height))
     }
 
+    // split Rectangle into equal sized left & right rectangles
     pub fn split_vertical(self) -> (Rect, Rect) {
+
+        let left_rect = Rect {
+            origin: Point { x: self.origin.x, y: self.origin.y },
+            size: Size { width: self.size.width / 2., height: self.size.height }
+        };
+        let right_rect = Rect {
+            origin: Point { x: self.origin.x + self.size.width / 2., y: self.origin.y },
+            size: Size { width: self.size.width / 2., height: self.size.height }
+        };
         (
-            Rect{
-                origin: Point{ x: self.origin.x, y: self.origin.y },
-                size: Size { width: self.size.width, height: self.size.height / 2. }},
-            Rect{
-                origin: Point { x: self.origin.x, y: self.size.height / 2. },
-                size: Size { width: self.size.width, height: self.size.height / 2. }}
+            left_rect,
+            right_rect
         )
     }
 
+    // split Rect into equal sized top & bottom rectangles
+    // Origin of Rects are bottom left (Screen dimensions) so
+    // bottom is with lower Y coordinates
     pub fn split_horizontal(self) -> (Rect, Rect) {
+        let bottom_rect = Rect {
+            origin: Point { x: self.origin.x, y: self.origin.y },
+            size: Size { width: self.size.width, height: self.size.height / 2. }
+        };
+        let top_rect = Rect {
+            origin: Point { x: self.origin.x, y: self.origin.y + self.size.height / 2.0 },
+            size: Size { width: self.size.width, height: self.size.height / 2. }
+        };
         (
-            Rect{
-                origin: Point{ x: self.origin.x, y: self.origin.y },
-                size: Size { width: self.size.width / 2., height: self.size.height }},
-            Rect{
-                origin: Point { x: self.size.width / 2., y: self.origin.y },
-                size: Size { width: self.size.width / 2., height: self.size.height }}
+            top_rect,
+            bottom_rect
         )
     }
 
